443deedf70b7b1a04e066892ac41a044cb108c47
Jacob Bare <jacob@parameter1.com>
2021-04-08 15:28:45 -0500

    Update app url and sending domain vars

--- services/membership/src/env.js
+++ services/membership/src/env.js
@@ -8,11 +8,11 @@ const {
 const { nonemptystr } = validators;
 
 module.exports = cleanEnv(process.env, {
-  APPLICATION_URL: nonemptystr({ desc: 'The URI of the management app', default: 'https://identity-x.base-cms.io' }),
+  APPLICATION_URL: nonemptystr({ desc: 'The URI of the management app', default: 'https://identity-x.parameter1.com' }),
   EXTERNAL_PORT: port({ desc: 'The external port that the service is exposed on.', default: 80 }),
   INTERNAL_PORT: port({ desc: 'The internal port that the service will run on.', default: 80 }),
   MONGO_DSN: nonemptystr({ desc: 'The MongoDB DSN to connect to.' }),
   NEW_RELIC_ENABLED: bool({ desc: 'Whether New Relic is enabled.', default: true, devDefault: false }),
   NEW_RELIC_LICENSE_KEY: nonemptystr({ desc: 'The license key for New Relic.', devDefault: '(unset)' }),
-  SENDING_DOMAIN: nonemptystr({ desc: 'The domain emails will be sent from.', default: 'identity-x.base-cms.io' }),
+  SENDING_DOMAIN: nonemptystr({ desc: 'The domain emails will be sent from.', default: 'identity-x.parameter1.com' }),
 });
