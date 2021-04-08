587745d8515531fad762870e22f2b9aa9f7fdc31
Jacob Bare <jacob@parameter1.com>
2021-04-08 14:55:54 -0500

    Fix lint errors

--- packages/mongoose-plugins/src/domain-validator.js
+++ packages/mongoose-plugins/src/domain-validator.js
@@ -4,7 +4,7 @@ const { isArray } = Array;
 
 module.exports = {
   validator(value) {
-    if (isArray(value)) return value.every(v => validator.isFQDN(v));
+    if (isArray(value)) return value.every((v) => validator.isFQDN(v));
     return validator.isFQDN(value);
   },
   message: 'Invalid domain name address {VALUE}',
