3be8a5f57bb0dccbd84a08188c2b1eb46fa24af7
Jacob Bare <jacob@parameter1.com>
2021-04-09 12:37:43 -0500

    Split token signing into own action

--- services/token/src/actions/index.js
+++ services/token/src/actions/index.js
@@ -1,9 +1,11 @@
 const create = require('./create');
 const invalidate = require('./invalidate');
+const sign = require('./sign');
 const verify = require('./verify');
 
 module.exports = {
   create,
   invalidate,
+  sign,
   verify,
 };
