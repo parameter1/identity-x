1ca97287b15f9c31457126c0f9a55103875dea29
Jacob Bare <jacob@parameter1.com>
2021-11-11 09:42:33 -0600

    Also check identifier value

--- services/application/src/actions/field/utils/prepare-external-id.js
+++ services/application/src/actions/field/utils/prepare-external-id.js
@@ -2,7 +2,7 @@ const EntityID = require('../../../entity-id/id');
 
 module.exports = (eid) => {
   const hasExternalId = eid
-    && eid.namespace && eid.namespace.type && eid.identifier && eid.identifier;
+    && eid.namespace && eid.namespace.type && eid.identifier && eid.identifier.value;
   if (!hasExternalId) return null;
   const externalId = new EntityID(eid.identifier, eid.namespace);
   return {
