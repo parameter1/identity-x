c7e6b60a59131c713c17d21ec0a3ac7ab4474192
Brian Miller <brian@parameter1.com>
2022-01-12 13:28:03 -0600

    udpate to custom question answer updates
    Instead of setting the custom select and boolean fields on app user update to the ones being currently sent(active) to also included previously answered, nonActive, questions.  This prevents lossing legacy data.

--- services/application/src/actions/user/update-custom-boolean-answers.js
+++ services/application/src/actions/user/update-custom-boolean-answers.js
@@ -21,9 +21,24 @@ module.exports = async ({
   // do not update user answers when passed answers are not an array
   if (!isArray(answers)) return user;
 
-  const toSet = answers
-    // .filter(({ value }) => optionIds.length) // ignore/unset fields without options
-    .map(({ fieldId, value }) => ({ _id: fieldId, value }));
+  // get all current answers as object { id, value }
+  const userObj = user.customBooleanFieldAnswers.reduce(
+    (obj, item) => Object.assign(obj, { [item._id]: item.value }), {},
+  );
+
+  // get new answers as object { id, value }
+  const newAnswers = answers.reduce(
+    (obj, item) => Object.assign(obj, { [item.fieldId]: item.value }), {},
+  );
+
+  // merge new and old ansers to account for old non active answers
+  const mergedAnwsers = { ...userObj, ...newAnswers };
+
+  // convert merged answers into valid array of { _id, value } answers
+  const toSet = Object.keys(mergedAnwsers).map((key) => {
+    const obj = { _id: key, value: mergedAnwsers[key] };
+    return obj;
+  });
 
   user.set('customBooleanFieldAnswers', toSet);
   if (profileLastVerifiedAt) {
--- services/application/src/actions/user/update-custom-select-answers.js
+++ services/application/src/actions/user/update-custom-select-answers.js
@@ -21,9 +21,26 @@ module.exports = async ({
   // do not update user answers when passed answers are not an array
   if (!isArray(answers)) return user;
 
-  const toSet = answers
+  // get all current answers as object { id, value }
+  const userObj = user.customSelectFieldAnswers.reduce(
+    (obj, item) => Object.assign(obj, { [item._id]: item.values }), {},
+  );
+
+  const newAnswers = answers
     .filter(({ optionIds }) => optionIds.length) // ignore/unset fields without options
-    .map(({ fieldId, optionIds }) => ({ _id: fieldId, values: optionIds }));
+    .map(({ fieldId, optionIds }) => ({ _id: fieldId, values: optionIds }))
+    .reduce(
+      (obj, item) => Object.assign(obj, { [item._id]: item.values }), {},
+    );
+
+  // merge new and old ansers to account for old non active answers
+  const mergedAnwsers = { ...userObj, ...newAnswers };
+
+  // convert merged answers into valid array of { _id, values } answers
+  const toSet = Object.keys(mergedAnwsers).map((key) => {
+    const obj = { _id: key, values: mergedAnwsers[key] };
+    return obj;
+  });
 
   user.set('customSelectFieldAnswers', toSet);
   if (profileLastVerifiedAt) {
