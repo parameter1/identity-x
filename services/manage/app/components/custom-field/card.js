5dd2ddcd79904e6ba0e4849c2305e35702b3feea
Jacob Bare <jacob@parameter1.com>
2022-01-17 11:35:44 -0600

    Display field type on custom field card

--- services/manage/app/components/custom-field/card.js
+++ services/manage/app/components/custom-field/card.js
@@ -8,4 +8,8 @@ export default Component.extend({
     const options = this.get('field.options');
     return options ? options.map((option) => option.label) : null;
   }),
+
+  type: computed('field.__typename', function() {
+    return this.get('field.__typename').replace(/Field$/, '');
+  }),
 });
