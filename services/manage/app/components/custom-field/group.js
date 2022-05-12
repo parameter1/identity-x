56fe46eeabf681bd0f64b275d2b10d7885582e38
Josh Worden <josh@parameter1.com>
2022-05-12 13:43:49 -0500

    Move custom logic to group component

--- services/manage/app/components/custom-field/group.js
+++ services/manage/app/components/custom-field/group.js
@@ -1,12 +1,30 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 
 const { isArray } = Array;
 
 export default Component.extend({
   disabled: false,
 
+  selectedOptions: computed('model.optionIds.[]', function() {
+    const optionIds = this.get('model.optionIds');
+    const options = this.get('options') || [];
+    return options.filter((option) => optionIds.includes(option.id));
+  }),
+
+  formattedOptions: computed('options.[]', function() {
+    const optionIds = this.get('model.optionIds');
+    return this.get('options').filter((option) => option.id && !optionIds.includes(option.id));
+  }),
+
   init() {
     this._super(...arguments);
     if (!isArray(this.options)) this.set('options', []);
   },
+
+  actions: {
+    handleChange(selection) {
+      this.set('model.optionIds', selection.map((option) => option.id));
+    }
+  }
 });
--- services/manage/app/components/custom-field/groups.js
+++ services/manage/app/components/custom-field/groups.js
@@ -13,7 +13,7 @@ export default Component.extend({
 
   actions: {
     add() {
-      this.groups.pushObject({ label: '' });
+      this.groups.pushObject({ label: '', optionIds: [] });
     },
   },
 });
