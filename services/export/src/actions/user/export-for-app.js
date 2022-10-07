99bc95548f6ddcf349753ac89773a32cdfd2bfc1
Shinsina <shinsina@att.net>
2022-10-07 08:10:35 -0500

    Correct fieldname

--- services/export/src/actions/user/export-for-app.js
+++ services/export/src/actions/user/export-for-app.js
@@ -20,7 +20,7 @@ module.exports = async ({
     'verified',
     'receiveEmail',
     'lastLoggedIn',
-    'profileLastVerified',
+    'profileLastVerifiedAt',
     'givenName',
     'familyName',
     'organization',
