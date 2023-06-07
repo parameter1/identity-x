parameter1/identity-x

commit d80444b5956576caf81f4986d541d6a7cac6f87e

Support setting write-in values

services/application/src/actions/field/user-select-answers.js
@@ -35,7 +35,15 @@ module.exports = async ({
     const fieldAnswer = customFieldAnswers.find(answer => `${answer._id}` === `${field._id}`);
     const answeredOptions = fieldAnswer && isArray(fieldAnswer.values)
       ? fieldAnswer.values
-        .map(value => field.options.find(option => `${option._id}` === `${value}`))
+        .map((value) => {
+          const option = field.options.find(o => `${o._id}` === `${value}`);
+          const writeInValue = fieldAnswer.writeInValues.find(v => `${v._id}` === `${value}`);
+          return {
+            ...option,
+            option,
+            ...(option.canWriteIn && writeInValue && { writeInValue: writeInValue.value }),
+          };
+        })
         .filter(option => option)
       : [];
 

services/graphql/src/graphql/definitions/app-user.js
@@ -146,7 +146,21 @@ type AppUserCustomSelectFieldAnswer {
   "Whether the user has answered the question."
   hasAnswered: Boolean!
   "The answered field option(s). This will always be an array, even if the field is a single-select only. An empty value signifies a non, or no longer valid, answer. It's up to the implementing components to account for this."
-  answers: [SelectFieldOption!]!
+  answers: [SelectFieldOptionAnswer!]!
+}
+
+type SelectFieldOptionAnswer {
+  "BC support: The select option ID. Also used as the option value."
+  id: String!
+  "The option that was selected"
+  option: SelectFieldOption!
+  "The write-in value, if supported by the option."
+  writeInValue: String
+
+  "BC support: The select option label. This is the value the user will see within the form control."
+  label: String!
+  "BC support: The external identifier value for this option. Only used when an external ID + namespace is associated with this field."
+  externalIdentifier: String
 }
 
 type AppUserConnection @projectUsing(type: "AppUser") {

services/graphql/src/graphql/resolvers/app-user.js
@@ -77,10 +77,16 @@ module.exports = {
         sort,
       });
       return selectFieldAnswers.map((selectFieldAnswer) => {
-        const { field } = selectFieldAnswer;
+        const { field, answers } = selectFieldAnswer;
         return {
           ...selectFieldAnswer,
-          answers: selectFieldAnswer.answers.map(answer => ({ field, option: answer })),
+          answers: answers.map(answer => ({
+            field,
+            // BC support
+            ...answer.option,
+            option: answer.option,
+            writeInValue: answer.writeInValue,
+          })),
         };
       });
     },

services/graphql/src/graphql/resolvers/field.js
@@ -83,24 +83,25 @@ module.exports = {
     /**
      *
      */
-    id: ({ option }) => option._id,
-
-    /**
-     *
-     */
-    label: ({ option }) => option.label,
-
+    id: option => option._id,
     /**
      *
      */
     externalIdentifier: ({ field, option }) => (
       field.externalId ? option.externalIdentifier : null
     ),
+  },
 
+  /**
+   *
+   */
+  SelectFieldOptionAnswer: {
     /**
-     * Whether or not the option supports a write-in (free-form text) value
+     * Legacy support for the old answer format
      */
-    canWriteIn: ({ option }) => option.canWriteIn,
+    id: ({ option }) => option._id,
+    label: ({ option }) => option.label,
+    externalIdentifier: ({ option }) => option.externalIdentifier,
   },
 
   /**

commit 1af7c699f1824b3b5765b6416173443f521cb44d

Don’t return missing/deleted options
Fixes error introduced in d80444b

services/application/src/actions/field/user-select-answers.js
@@ -37,6 +37,10 @@ module.exports = async ({
       ? fieldAnswer.values
         .map((value) => {
           const option = field.options.find(o => `${o._id}` === `${value}`);
+          if (!option) {
+            // Don't return deleted/missing options
+            return false;
+          }
           const writeInValue = fieldAnswer.writeInValues.find(v => `${v._id}` === `${value}`);
           return {
             ...option,