81a3274023a9689028397285e9b3e65a5dc701c5
Brian Miller <brian@parameter1.com>
2023-07-07 11:45:06 -0500

    Add initial template overrides for Watt groups
    Add watt global media initial english template overrides with a simple subject line change of `Your personal ${Domain} login link`

---
+++ services/application/src/email-templates/login-link/644960504a7ef94021814608/index.js
@@ -0,0 +1,5 @@
+const englishTemplate = require('./en-us');
+
+module.exports = {
+  'en-us': englishTemplate,
+};
--- services/application/src/email-templates/login-link/index.js
+++ services/application/src/email-templates/login-link/index.js
@@ -1,7 +1,16 @@
 const englishTemplate = require('./en-us');
 const spanishTemplate = require('./es-mx');
+const wattGlobalMediaFG = require('./644960484a7ef9f2d2814607');
+const wattGlobalMediaFS = require('./644960504a7ef94021814608');
+const wattGlobalMediaPF = require('./644960610f24dbebcd6c59e7');
+const wattGlobalMediaAG = require('./64496069361977a149b5e3a2');
+
 
 module.exports = {
   'en-us': englishTemplate,
   'es-mx': spanishTemplate,
+  '644960484a7ef9f2d2814607': wattGlobalMediaFG,
+  '644960504a7ef94021814608': wattGlobalMediaFS,
+  '644960610f24dbebcd6c59e7': wattGlobalMediaPF,
+  '64496069361977a149b5e3a2': wattGlobalMediaAG,
 };
