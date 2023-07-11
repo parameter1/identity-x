3b462fc530232f98e4f99e9aaa2328b3c6dd6b16
Brian Miller <brian@parameter1.com>
2023-07-11 13:20:38 -0500

    udpate template

--- services/application/src/email-templates/login-link/es-mx.js
+++ services/application/src/email-templates/login-link/es-mx.js
@@ -1,34 +1,44 @@
+const stripTags = require('striptags');
+
 module.exports = ({
   sendingDomain,
   supportEmail,
   url,
   appName,
   addressValues,
-} = {}) => ({
-  html: `
-  <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-  <html lang="es">
-    <head>
-      <meta charset="UTF-8">
-      <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-      <meta name="viewport" id="viewport" content="width=device-width,minimum-scale=1.0,maximum-scale=10.0,initial-scale=1.0">
-      <title>Su enlace de inicio de sesión personal</title>
-    </head>
-    <body>
-      <p>Recientemente solicitó iniciar sesión en <strong>${appName}</strong>. Este enlace esta habilitado por una hora y caducará inmediatamente después de su uso.</p>
-      <p><a href="${url}">Iniciar sesión en ${appName}</a></p>
-      <p>Si no solicitó este enlace, simplemente ignore este correo electrónico${supportEmail ? ` o <a href="mailto:${supportEmail}">comuníquese con nuestro personal de soporte</a>` : ''}.</p>
-      <hr>
-      <small style="font-color: #ccc;">
-        <p>Agregue <em>${sendingDomain}</em> a su libreta de direcciones o lista de remitentes seguros para asegurarse de recibir nuestros correos electrónicos en el futuro</p>
-        <p>Está recibiendo este correo electrónico porque se realizó una solicitud de inicio de sesión en ${appName}.</p>
-        <p>Para obtener información adicional, comuníquese con ${appName} ${addressValues.join(', ')}.</p>
-      </small>
-    </body>
-  </html>
-`,
-  text: `
-Recientemente solicitó iniciar sesión en ${appName}. Este enlace esta habilitado por una hora y caducará inmediatamente después de su uso.
+  loginLinkTemplate,
+  user,
+} = {}) => {
+  const { verified } = user;
+  const subject = loginLinkTemplate.subject ? loginLinkTemplate.subject : 'Su enlace de inicio de sesión personal';
+  const unverifiedVerbiage = loginLinkTemplate.unverifiedVerbiage ? loginLinkTemplate.unverifiedVerbiage : `Recientemente solicitó iniciar sesión en <strong>${appName}</strong>. Este enlace esta habilitado por una hora y caducará inmediatamente después de su uso.`;
+  const verifiedVerbiage = loginLinkTemplate.verifiedVerbiage ? loginLinkTemplate.verifiedVerbiage : `Recientemente solicitó iniciar sesión en <strong>${appName}</strong>. Este enlace esta habilitado por una hora y caducará inmediatamente después de su uso.`;
+  const verbiage = verified ? verifiedVerbiage : unverifiedVerbiage;
+  return ({
+    html: `
+    <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+    <html lang="es">
+      <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <meta name="viewport" id="viewport" content="width=device-width,minimum-scale=1.0,maximum-scale=10.0,initial-scale=1.0">
+        <title>Su enlace de inicio de sesión personal</title>
+      </head>
+      <body>
+        <p>${verbiage}</p>
+        <p><a href="${url}">Iniciar sesión en ${appName}</a></p>
+        <p>Si no solicitó este enlace, simplemente ignore este correo electrónico${supportEmail ? ` o <a href="mailto:${supportEmail}">comuníquese con nuestro personal de soporte</a>` : ''}.</p>
+        <hr>
+        <small style="font-color: #ccc;">
+          <p>Agregue <em>${sendingDomain}</em> a su libreta de direcciones o lista de remitentes seguros para asegurarse de recibir nuestros correos electrónicos en el futuro</p>
+          <p>Está recibiendo este correo electrónico porque se realizó una solicitud de inicio de sesión en ${appName}.</p>
+          <p>Para obtener información adicional, comuníquese con ${appName} ${addressValues.join(', ')}.</p>
+        </small>
+      </body>
+    </html>
+  `,
+    text: `
+${stripTags(verbiage)}
 
 Iniciar sesión en ${appName}:
 ${url}
@@ -40,6 +50,7 @@ Si no solicitó este enlace, simplemente ignore este correo electrónico${suppor
 Agregue ${sendingDomain} a su libreta de direcciones o lista de remitentes seguros para asegurarse de recibir nuestros correos electrónicos en el futuro
 Usted esta recibiendo este correo porque una solicitud fue hecha a ${appName}.
 Para obtener información adicional, comuníquese con ${appName} ${addressValues.join(', ')}.
-`,
-  subject: 'Su enlace de inicio de sesión personal',
-});
+  `,
+    subject,
+  });
+};
