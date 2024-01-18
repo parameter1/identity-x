f710e7417948f43ebdb910660330da677c3b61c0
Josh Worden <josh@parameter1.com>
2024-01-18 16:08:52 -0600

    segment import, user cli update

---
+++ services/cli/src/actions/create-segment-fg.js
@@ -0,0 +1,83 @@
+const { applicationService } = require('@identity-x/service-clients');
+const { eachSeries } = require('async');
+
+const { log } = console;
+
+module.exports = async () => {
+  const ps = [
+    { name: 'FG - Operations - Convey Handling - Aeration Dry', rules: [] },
+    { name: 'FG - Mfg Equip - Batching CRM Automation Software', rules: [] },
+    { name: 'FG - Facility Design & Construction', rules: [] },
+    { name: 'FG - Testing and Analysis', rules: [] },
+    { name: 'FG - Feed Transportation Equipment', rules: [] },
+  ];
+  const questionIds = ['643404c587747e1f4434a8c8', '6430a9a27694b7652a933308'];
+  const personasToAssign = {
+    '643405a83e8177091cdc7f15_6430aa6653778299501b85c1': [ps[0], ps[1], ps[2], ps[3], ps[4]],
+    '643405a83e8177091cdc7f15_6430aa6653778251051b85c0': [ps[0], ps[1], ps[2], ps[3], ps[4]],
+    '643405a83e8177091cdc7f15_6430aa66537782178d1b85bf': [ps[1], ps[3]],
+    '643405a83e8177091cdc7f15_6430aa665377823c961b85be': [ps[0], ps[1], ps[2], ps[3], ps[4]],
+    '643405a83e81777bd8dc7f14_6430aa6653778299501b85c1': [ps[0], ps[1], ps[2], ps[3], ps[4]],
+    '643405a83e81777bd8dc7f14_6430aa6653778251051b85c0': [ps[0], ps[1], ps[2], ps[3], ps[4]],
+    '643405a83e81777bd8dc7f14_6430aa66537782178d1b85bf': [ps[1], ps[3]],
+    '643405a83e81777bd8dc7f14_6430aa665377823c961b85be': [ps[0], ps[1], ps[2], ps[3], ps[4]],
+    '643405a83e81773876dc7f16_6430aa6653778299501b85c1': [ps[0], ps[1], ps[2], ps[3], ps[4]],
+    '643405a83e81773876dc7f16_6430aa6653778251051b85c0': [ps[0], ps[1], ps[2], ps[3], ps[4]],
+    '643405a83e81773876dc7f16_6430aa66537782178d1b85bf': [ps[1], ps[3]],
+    '643405a83e81773876dc7f16_6430aa665377823c961b85be': [ps[0], ps[1], ps[2], ps[3], ps[4]],
+    '643405a83e81770ec7dc7f0b_6430aa6653778299501b85c1': [ps[0], ps[1], ps[2], ps[3], ps[4]],
+    '643405a83e81770ec7dc7f0b_6430aa6653778251051b85c0': [ps[0], ps[1], ps[2], ps[3], ps[4]],
+    '643405a83e81770ec7dc7f0b_6430aa66537782178d1b85bf': [ps[1], ps[3]],
+    '643405a83e81770ec7dc7f0b_6430aa665377823c961b85be': [ps[0], ps[1], ps[2], ps[3], ps[4]],
+    '643405a83e81772ceedc7f17_6430aa6653778299501b85c1': [ps[0], ps[2], ps[3], ps[4]],
+    '643405a83e81772ceedc7f17_6430aa6653778251051b85c0': [ps[0], ps[2], ps[3], ps[4]],
+    '643405a83e81772ceedc7f17_6430aa66537782178d1b85bf': [ps[3]],
+    '643405a83e81772ceedc7f17_6430aa665377823c961b85be': [ps[0], ps[2], ps[3], ps[4]],
+    '643405a83e81772253dc7f0a_6430aa6653778299501b85c1': [ps[0], ps[1], ps[2], ps[3], ps[4]],
+    '643405a83e81772253dc7f0a_6430aa6653778251051b85c0': [ps[0], ps[1], ps[2], ps[3], ps[4]],
+    '643405a83e81772253dc7f0a_6430aa66537782178d1b85bf': [ps[1], ps[3]],
+    '643405a83e81772253dc7f0a_6430aa665377823c961b85be': [ps[0], ps[1], ps[2], ps[3], ps[4]],
+    '643405a83e81775863dc7f13_6430aa6653778299501b85c1': [ps[0], ps[1]],
+    '643405a83e81775863dc7f13_6430aa6653778251051b85c0': [ps[0], ps[1]],
+    '643405a83e81775863dc7f13_6430aa66537782178d1b85bf': [ps[1]],
+    '643405a83e81775863dc7f13_6430aa665377823c961b85be': [ps[0], ps[1]],
+    '643405a83e81775a33dc7f12_6430aa6653778299501b85c1': [ps[0], ps[1]],
+    '643405a83e81775a33dc7f12_6430aa6653778251051b85c0': [ps[0], ps[1]],
+    '643405a83e81775a33dc7f12_6430aa66537782178d1b85bf': [ps[1]],
+    '643405a83e81775a33dc7f12_6430aa665377823c961b85be': [ps[0], ps[1]],
+    '643405a83e8177fa68dc7f0f_6430aa6653778299501b85c1': [ps[0], ps[1], ps[2], ps[3], ps[4]],
+    '643405a83e8177fa68dc7f0f_6430aa6653778251051b85c0': [ps[0], ps[1], ps[2], ps[3], ps[4]],
+    '643405a83e8177fa68dc7f0f_6430aa66537782178d1b85bf': [ps[1], ps[3]],
+    '643405a83e8177fa68dc7f0f_6430aa665377823c961b85be': [ps[0], ps[1], ps[2], ps[3], ps[4]],
+    '643405a83e817783a6dc7f10_6430aa6653778299501b85c1': [ps[0], ps[1], ps[2], ps[3], ps[4]],
+    '643405a83e817783a6dc7f10_6430aa6653778251051b85c0': [ps[0], ps[1], ps[2], ps[3], ps[4]],
+    '643405a83e817783a6dc7f10_6430aa66537782178d1b85bf': [ps[1], ps[3]],
+    '643405a83e817783a6dc7f10_6430aa665377823c961b85be': [ps[0], ps[1], ps[2], ps[3], ps[4]],
+    '643405a83e81776603dc7f11_6430aa6653778299501b85c1': [ps[0], ps[1], ps[2], ps[3], ps[4]],
+    '643405a83e81776603dc7f11_6430aa6653778251051b85c0': [ps[0], ps[1], ps[2], ps[3], ps[4]],
+    '643405a83e81776603dc7f11_6430aa66537782178d1b85bf': [ps[1], ps[3]],
+    '643405a83e81776603dc7f11_6430aa665377823c961b85be': [ps[0], ps[1], ps[2], ps[3], ps[4]],
+  };
+
+  [...Object.entries(personasToAssign)].forEach(([key, personas]) => {
+    const answerIds = key.split('_');
+    personas.forEach((persona) => {
+      persona.rules.push({
+        conditions: [
+          { field: questionIds[0], answer: answerIds[0] },
+          { field: questionIds[1], answer: answerIds[1] },
+        ],
+      });
+    });
+  });
+
+  await eachSeries(ps, async (segment) => {
+    const r = await applicationService.request('segment.create', {
+      applicationId: '6449537d36197792dcb5e367',
+      payload: segment,
+    });
+    console.log(segment, r);
+  });
+
+  log('All segments created!');
+};
--- services/cli/src/actions/save-user.js
+++ services/cli/src/actions/save-user.js
@@ -1,15 +1,36 @@
 const inquirer = require('inquirer');
-const { applicationService } = require('@identity-x/service-clients');
+const { applicationService, membershipService, organizationService } = require('@identity-x/service-clients');
 
 const { log } = console;
 
 module.exports = async () => {
-  const { email, applicationId } = await inquirer.prompt([
+  const {
+    email,
+    applicationId,
+  } = await inquirer.prompt([
+    {
+      name: 'organizationId',
+      message: 'Which organization?',
+      type: 'list',
+      default: '63e974eb0d243edfbe501e80', // WATT
+      choices: async () => {
+        log('Loading organizations...');
+        const memberships = await membershipService.request('listForUser', { email: 'josh@parameter1.com' });
+        const organizations = await Promise.all(memberships.map(
+          ({ organizationId }) => organizationService.request('findById', { id: organizationId, fields: ['name'] }),
+        ));
+        return organizations.map(org => ({ name: org.name, value: `${org._id}` }));
+      },
+    },
     {
       name: 'applicationId',
       message: 'Which application id?',
-      type: 'input',
-      default: '6449537d36197792dcb5e367',
+      type: 'list',
+      default: '6449537d36197792dcb5e367', // All Websites
+      choices: async (ans) => {
+        const opts = await applicationService.request('listForOrg', { id: ans.organizationId });
+        return opts.map(app => ({ name: app.name, value: `${app._id}` }));
+      },
     },
     {
       name: 'email',
