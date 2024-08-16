fd6569891346e148bec2efcc4991cc15f9165194
Josh Worden <josh@parameter1.com>
2024-08-16 09:49:18 -0500

    fix spacing, update to use nested dropdown

--- services/manage/app/controllers/manage/orgs/view/apps/view/users/edit.js
+++ services/manage/app/controllers/manage/orgs/view/apps/view/users/edit.js
@@ -2,9 +2,19 @@ import Controller from '@ember/controller';
 import ActionMixin from '@identity-x/manage/mixins/action-mixin';
 import AppQueryMixin from '@identity-x/manage/mixins/app-query';
 import { inject } from '@ember/service';
+import { computed } from '@ember/object';
 
 export default Controller.extend(ActionMixin, AppQueryMixin, {
   router: inject(),
+
+  isCustomActive: computed('router.currentRouteName', function() {
+    return this.router.currentRouteName.includes('users.edit.custom-');
+  }),
+  customDropdownClasses: computed('isCustomActive', function() {
+    const classes = ['nav-link', 'dropdown-toggle'];
+    if (this.isCustomActive) classes.push('active');
+    return classes.join(' ');
+  }),
 
   actions: {
     returnToList() {
